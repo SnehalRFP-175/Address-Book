b3c456b3835d87143a4480173b02a3030a9775db
Snehal <nanavaresnehal115@gmail.com>
2022-08-24 10:09:01 +0530

    ADDED[Snehal] UC-3 Updating the Contact of Address Book

--- src/com/AddressBook/BridgeLabz/AddressBook.java
+++ src/com/AddressBook/BridgeLabz/AddressBook.java
@@ -1,27 +1,127 @@
 package com.AddressBook.BridgeLabz;
 
-/*
- * UC2
- * Ability to create Contacts in Address Book with First and Last Name, Address, City, State
- * Zip Code, Phone Number,Email
- */
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressBook extends Contacts
+public class AddressBook 
 {
 	
 	Scanner sc = new Scanner(System.in);
 	
+	static ArrayList<Contacts> contactList = new ArrayList<>();
+	
+	
 	//add methode for Adding new Contact
-		public void addDetails()
-		{
-			Contacts contact = new Contacts();
-			
-			contactDetails();			
-		}
+	public void addContacts()
+	{
+		System.out.println("Enter the Number of Contacts You Want to Add :");
+		int num=sc.nextInt();
 		
-		public void display()
+		for(int i = 0 ; i < num ; i++)
 		{
-			System.out.println(toString());
+			
+			Contacts contact = new Contacts(); //Local Object
+			
+			contact.contactDetails();	
+			contactList.add(contact);
 		}
+
+	}
+	
+	// display method to display all contact details
+	public void displayContacts()
+	{
+		for (int i = 0; i < contactList.size(); i++) {
+			 Contacts contact = contactList.get(i);
+			 System.out.println(contact.toString());
+	        }
+	}
+	
+	
+	//update method to update the contacts details
+	  public void updateContacts()
+	{
+		System.out.println("Enter Name to Update the Information :");
+		String firstName = sc.next();
+		boolean isAvailable = false;
+		
+		for (Contacts contact : contactList) {
+			
+			  if(contact.getFirstName().equals(firstName))
+			  { 
+				  while(true)
+				{
+				  isAvailable = true;
+				  System.out.println(" 1. First Name \n 2. Last Name \n 3. Address \n 4. City \n 5. State \n 6. Zip Code \n 7. Mobile Number \n 8. Email Id \n 9. ALL \n 10. EXIT");
+				  System.out.println("What you want to edit :");
+				  
+				  int update =sc.nextInt(); 
+				
+				  switch(update)
+				  {
+				  case 1:
+					  System.out.println("\nEnter First Name :");
+					  contact.setFirstName(sc.next());
+					  break;
+					  
+				  case 2:
+					  System.out.println("Enter Last Name :");
+					  contact.setLastName(sc.next());
+					  break;
+					  
+				  case 3:
+					  System.out.println("Enter Your Address :");
+					  contact.setAddress(sc.next());
+					  break;
+					  
+				  case 4:
+					  System.out.println("Enter Your City:");
+					  contact.setCity(sc.next());
+					  break;
+					  
+				  case 5:
+					  System.out.println("Enter State :");
+					  contact.setState(sc.next());
+					  break;
+					  
+				  case 6:
+					  System.out.println("Enter Zip Code :");
+					  contact.setZip(sc.next());
+					  break;
+					  
+				  case 7:
+					  System.out.println("Enter Mobile Number :");
+					  contact.setPhoneNumber(sc.next());
+					  break;
+					  
+				  case 8:
+					  System.out.println("Enter Email Id :");
+					  contact.setEmail(sc.next());
+					  break;
+					  
+				  case 9:
+					  contact.contactDetails();
+					  System.out.println("Updated Sucessfully....!");
+					  return;
+					  
+				  case 10:
+					  System.out.println("Exited...!");
+					  return;
+					  
+				  default:
+					  System.out.println("Enter valid Choice..!");
+					  break;
+						  
+				  }
+				  System.out.println("Updated Sucessfully....!");
+			  }	
+		}
+	}
+		 if( !isAvailable )
+		  {
+			  System.out.println("Contact Not Available....!");
+		  }
+		
+		
+	}
 }		
